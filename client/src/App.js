7ce516f27313b8b7fef850c8c199902395569fb2
Anh Le Duc <wolfwithcode@gmail.com>
2020-12-18 10:08:25 +0700

    Add logout function

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,71 @@
-import React from 'react';
-import './App.css'
-import axios from 'axios'
-axios.defaults.withCredentials = true
+import React from "react";
+import "./App.css";
+import axios from "axios";
+axios.defaults.withCredentials = true;
 
-
-const LOCAL_HOST = 'http://localhost:3777/api'
-const REMOTE_HOST = 'https://haisanmo3-backend.herokuapp.com/api'
+const LOCAL_HOST = "http://localhost:3777/api";
+const REMOTE_HOST = "https://haisanmo3-backend.herokuapp.com/api";
 // const HOST = 'https://haisanmo3-backend.herokuapp.com/api'
 const HOST = LOCAL_HOST;
 
-const email = 'anhld1111@g.com';
-const username = 'anhld';
-const password = 'anhld';
+const email = "anhld1111@g.com";
+const username = "anhld";
+const password = "anhld";
 
-const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuaGxkMTExMUBnLmNvbSIsImlhdCI6MTYwODEwNzMxMCwiZXhwIjoxNjA4MTE0NTEwfQ.HfJtMGKJRkmqb-dZcsjSlxQCaRwJt2m9FqEJGSRCcGY';
-const refeshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuaGxkMTExMUBnLmNvbSIsImlhdCI6MTYwODE5MzkxOSwiZXhwIjoxNjA4MjAxMTE5fQ.BHAZnhjgzKvaVSCp8YdTS4Pe5-dQSFOTLTQtUT3-uPI'
-
+const accessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuaGxkMTExMUBnLmNvbSIsImlhdCI6MTYwODEwNzMxMCwiZXhwIjoxNjA4MTE0NTEwfQ.HfJtMGKJRkmqb-dZcsjSlxQCaRwJt2m9FqEJGSRCcGY";
+const refeshToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuaGxkMTExMUBnLmNvbSIsImlhdCI6MTYwODE5MzkxOSwiZXhwIjoxNjA4MjAxMTE5fQ.BHAZnhjgzKvaVSCp8YdTS4Pe5-dQSFOTLTQtUT3-uPI";
 
 const signIn = async () => {
   try {
-      const response = await axios.post(HOST+'/auth/signin', {
-          email,
-          username,
-          password,
-      })
-      // console.log('response ', response);
-      const {data} = response;
-      console.log('data', data);
-
+    const response = await axios.post(HOST + "/auth/signin", {
+      email,
+      username,
+      password,
+    });
+    // console.log('response ', response);
+    const { data } = response;
+    console.log("data", data);
   } catch (error) {
-      console.log('err ', error);
+    console.log("err ", error);
   }
-}
-
-
+};
 
 const refeshLogin = async () => {
-try {
-    const response = await axios.post(HOST+'/auth/refresh', {           
-        // refeshToken
-    })
-    // // console.log('response ', response);
-    // const {data} = response;
-    // console.log('data', data);
-    // console.log('response accesstoken', (response.headers));
-    // console.log('response refeshToken', JSON.stringify(response.headers));
+  try {
+    const response = await axios.post(HOST + "/auth/refresh");
+  } catch (error) {
+    console.log("err ", error);
+  }
+};
 
-} catch (error) {
-    console.log('err ', error);
-}
-}
-
-
-
+const logOut = async () => {
+  try {
+    const response = await axios.post(HOST + "/auth/logout");
+  } catch (error) {
+    console.log("err ", error);
+  }
+};
 
 function App() {
-
-
-  
-	return (
-		<div className="App">
+  return (
+    <div className="App">
       <h1>HTTP ONLY COOKIE DEMO</h1>
-			<div className="box">
-        <button className="button green" onClick={signIn}>signIn</button>
-        <button className="button yellow" onClick={refeshLogin}>refeshLogin</button>
+      <div className="box">
+        <button className="button green" onClick={signIn}>
+          signIn
+        </button>
+        <button className="button yellow" onClick={refeshLogin}>
+          refeshLogin
+        </button>
+        <button className="button yellow" onClick={logOut}>
+        logOut
+        </button>
         {/* <button className="button red" onClick={deleteCookie}>Delete Cookie</button> */}
       </div>
-		</div>
-	);
+    </div>
+  );
 }
 
 export default App;
